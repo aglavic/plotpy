e856c8a8eacd11cab0548c12687c6d84a7203edb
Artur Glavic <glavic@iff348.(none)>
2009-04-05 22:54:18 +0200

    Packageing

--- gnuplot_preferences_4circle-normal.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# Changes of gnuplot preferences specific for 4circle Data
-# Overrides all settings in gnuplot_preferences.py
-plotting_parameters='w lines lw 2'
-plotting_parameters_errorbars='w errorbars pt 5 ps 0.5 lw 2' # plotting with errorbars
-settings_3d='set pm3d interpolate 5,5\nset ticslevel 0.05\n'+\
-'set palette defined (0 "blue",50 "green", 80 "yellow",150 "red",200 "purple", 255 "black")\n'
--- gnuplot_preferences_reflectometer.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# Changes of gnuplot preferences specific for reflectometer Data
-# Overrides all settings in gnuplot_preferences.py
-plotting_parameters='w lines lw 2' # plotting x-y
-plotting_parameters_errorbars='w errorbars pt 5 ps 0.5 lw 2' # plotting with errorbars
