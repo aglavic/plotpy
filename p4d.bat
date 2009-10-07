966435021a4a8f65bae18b551fbf13ddfff68a4c
Artur Glavic <glavic@iff348.(none)>
2009-10-07 18:14:28 +0200

    Remove unused files, change install/uninstall scripts.

--- p4d.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-PATH=%PATH%;C:\python26;C:\plotting-script;C:\plotting-script\gnuplot\bin;C:\Program Files\gfortran\bin;C:\Programme\gfortran\bin
-python C:\plotting-script\plot.py 4circle -gs %*
