ddcc30073d6390419f403088110b467c99e24e36
Artur Glavic <glavic@iff348.(none)>
2010-05-18 11:03:28 +0200

    fix script

--- plot.py
+++ plot.py
@@ -4,6 +4,8 @@
   Dummy module to run the program.
 '''
 
-import __init__
+import sys, os
+sys.path.insert(0, os.path.split(__file__)[0])
+import __init__ as plot_script
 
-__init__.run()
+plot_script._run()
